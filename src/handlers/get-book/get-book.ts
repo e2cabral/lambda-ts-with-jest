721b20906aa8bbd52178102fe4095a8ee4765e43
Edson Cabral Junior <e2cabraljunior@gmail.com>
2022-01-07 21:12:28 -0300

    🚧 add a function to validate the path parameter

--- src/handlers/get-book/get-book.ts
+++ src/handlers/get-book/get-book.ts
@@ -1,22 +1,27 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { Book } from '../../domain/book.model';
 import { badRequest, ok } from '../../helpers/http-helpers';
+import { validateGetBook } from '../../validation/book/validator';
 
 export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const id = event.pathParameters?.id;
+  try {
+    const id = event.pathParameters?.id;
 
-  if (!id) return badRequest(new Error('An invalid id was provided.'));
+    validateGetBook(id);
 
-  const book: Book = new Book(
-    new Date(),
-    'Me',
-    230,
-    'A couple that need time to understand each other',
-    ['Laszlo', 'Otto'],
-    'Romance',
-    12.95,
-    5,
-  );
+    const book: Book = new Book(
+      new Date(),
+      'Me',
+      230,
+      'A couple that need time to understand each other',
+      ['Laszlo', 'Otto'],
+      'Romance',
+      12.95,
+      5,
+    );
 
-  return ok(book);
+    return ok(book);
+  } catch (err) {
+    return badRequest((err as Error));
+  }
 };
